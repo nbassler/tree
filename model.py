4470fb8eaa6c5a5b81ff52a1703910446e1a3da4
Niels Bassler <niels.bassler@gmail.com>
2018-05-05 13:06:52 +0200

    Setup minimal MVC

---
+++ model.py
@@ -0,0 +1,12 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class MyData(object):
+    foo = ['smerg', 'smerg']
+    bar = []
+    fofofo = "Test"
+
+    def __init__(self):
+        pass
---
+++ view.py
@@ -0,0 +1,26 @@
+import logging
+
+from PyQt5 import QtWidgets
+
+logger = logging.getLogger(__name__)
+
+
+class MyView(QtWidgets.QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        vbox = QtWidgets.QVBoxLayout(self)
+
+        btn = QtWidgets.QPushButton('Add Data', self)
+        tw = QtWidgets.QTreeWidget()
+
+        vbox.addWidget(tw)
+        vbox.addWidget(btn)
+        self.setLayout(vbox)
+
+        self.setWindowTitle('TreeWidget')
